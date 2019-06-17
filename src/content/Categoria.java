3a7baf5f3f3616bdf681da217e6c05bb4e77f26a
FelipeMoleiro <40440420+FelipeMoleiro@users.noreply.github.com>
2019-06-17 12:57:36 -0300

    Add files via upload

--- src/content/Categoria.java
+++ src/content/Categoria.java
@@ -1,35 +1,62 @@
 package content;
 
+/**
+ * Classe que representa uma categoria de um livro
+ *
+ */
 public class Categoria {
     private int id;
     private String nome;
-
-    public Categoria(String nome) {
-        this.id = 0;
-        this.nome = nome;
-    }
-
+    
+    /**
+     * Contrutor do objeto categoria que recebe o nome da categoria e um inteiro como chave para a categoria
+     * @param id - chave associada a categoria
+     * @param nome - nome da categoria
+     */
     public Categoria(int id, String nome) {
         this.id = id;
         this.nome = nome;
     }
-
+    
+    /**
+     * Função que retorna o valor contido em id
+     * @return inteiro com o valor do campo id
+     */
     public int getId() {
         return id;
     }
-
+    
+    /**
+     * Função que retorna o valor contido em nome
+     * @return String com o nome da categoria
+     */
     public String getNome() {
         return nome;
     }
-
+    
+   /**
+    * Função para mudar o id de um objeto Categoria
+    * @param id Inteiro que representa o novo id da categoria
+    */
     public void setId(int id) {
         this.id = id;
     }
-
+    
+    /**
+     * Função para mudar o nome de um objeto Categoria
+     * @param nome String que representa o novo nome da categoria
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+   
+    /**
+     * Função que transforma o objeto Categoria em String no seguinte formato:
+     * 		"(id) nome"
+     * 
+     * @return String que representa o objeto Categoria
+     */
     @Override
     public String toString() {
         return "(" + id + ") " + nome;
--- src/content/Livro.java
+++ src/content/Livro.java
@@ -1,15 +1,27 @@
 package content;
 
+/**
+ * Classe que representa um livro e suas caracteristicas
+ * int id - chave do livro
+ * Categoria categoria - categoria do livro
+ * String nome - nome do livro
+ * String autor - autor do livro
+ * String foto - caminho nos diretorios para arquivo da foto
+ *
+ */
 public class Livro {
     private int id;
     private Categoria categoria;
     private String nome, autor, foto;
 
-    public Livro() {
-        this.id = 0;
-    }
-
-
+    /**
+     * 
+     * @param id - inteiro com chave do livro
+     * @param categoria - objeto Categoria que representa a categoria do livro
+     * @param nome - String nome do livro
+     * @param autor  String autor do livro
+     * @param foto - String caminho nos diretorios para arquivo da foto
+     */
     public Livro(int id, Categoria categoria, String nome, String autor, String foto) {
         this.id = id;
         this.categoria = categoria;
@@ -18,52 +30,91 @@ public class Livro {
         this.foto = foto;
     }
 
+    /**
+     * Função que retorna o valor contido em id
+     * @return inteiro com o valor do campo id
+     */
     public int getId() {
         return id;
     }
-
+    
+    /**
+     * Função para mudar o id de um objeto Livro
+     * @param id Inteiro que representa o novo id do livro
+     */
     public void setId(int id) {
         this.id = id;
     }
-
+    
+    /**
+     * Função que retorna o objeto Categoria que representa a categoria do livro
+     * @return objeto Categoria que representa a categoria do livro
+     */
     public Categoria getCategoria() {
         return categoria;
     }
-
+    
+    /**
+     * Função para mudar a categoria de um objeto Livro
+     * @param categoria Objeto Categoria que sera a nova categoria do livro
+     */
     public void setCategoria(Categoria categoria) {
         this.categoria = categoria;
     }
-
-    public void setCategoria(int id, String nome) {
-        this.categoria = new Categoria(id, nome);
-    }
-
+    
+    /**
+     * Função que retorna o nome do livro
+     * @return String nome do livro
+     */
     public String getNome() {
         return nome;
     }
-
+    
+    /**
+     * Função para mudar o nome do Livro
+     * @param nome String nome que será o novo nome do livro
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
-
+    
+    /**
+     * Função que retorna o autor do livro
+     * @return String autor do livro
+     */
     public String getAutor() {
         return autor;
     }
-
+    
+    /**
+     * Função para mudar o autor do Livro
+     * @param autor String autor que será o novo autor do livro
+     */
     public void setAutor(String autor) {
         this.autor = autor;
     }
-
+    
+    /**
+     * Função que retorna a String caminho nos diretorios para arquivo da foto do livro
+     * @return String caminho nos diretorios para arquivo da foto
+     */
     public String getFoto() {
         return foto;
     }
-
+    
+    /**
+     * Função para mudar a foto do Livro
+     * @param foto String caminho nos diretorios para arquivo da foto
+     */
     public void setFoto(String foto) {
         this.foto = foto;
     }
-
+    
+    /**
+     * Função que cria uma representação em String do objeto Livro
+     */
     @Override
     public String toString() {
-        return "(" + id + ") " + nome + " - " + autor + " - " + categoria.getNome();
+        return "(" + id + ") " + nome + " - " + autor + " - " + categoria;
     }
 }
